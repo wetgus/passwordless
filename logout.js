910d3fa9cd7fe6aa99b0ce7aec3c38ca6416f2c0
wetgus <wetgusbetting@gmail.com>
2024-10-07 23:00:44 +0300

    Delete logout.js

--- logout.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('logout-button').addEventListener('click', function() {
-    localStorage.removeItem('deviceToken');
-    alert('You have been logged out.');
-    location.reload();  // Reload the page to show the registration dialogue
-});
